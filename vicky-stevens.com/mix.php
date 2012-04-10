c913aaf31d58e269b73f17a314469c9d70731d73
Vicky Stevens <bedlam@gmail.com>
2012-04-10 16:28:08 +0100

    first commit.  all sites

---
+++ vicky-stevens.com/mix.php
@@ -0,0 +1,36 @@
+{
+			"status": "OK",
+			"num_results": 3,
+			"results": [
+				{
+					"id": "212",
+					"lat": "50.842941",
+					"lng": "-0.141312",
+					"elevation": "0",
+					"title": "Guess PIM2",
+					"distance": "1.24",
+					"has_detail_page": "1",
+					"webpage": "http%3A%2F%2Fwww.mediafusion.web.id"
+				},
+				{
+					"id": "213",
+					"lat": "50.819583",
+					"lng": "-0.136420",
+					"elevation": "0",
+					"title": "iBox Plaza EX",
+					"distance": "10.24",
+					"has_detail_page": "1",
+					"webpage": "http%3A%2F%2Fwww.mediafusion.web.id"
+				},
+				{
+					"id": "215",
+					"lat": "-6.175030310631483",
+					"lng": "106.79054260253906",
+					"elevation": "0",
+					"title": "Red Mango GI",
+					"distance": "9.24",
+					"has_detail_page": "1",
+					"webpage": "http%3A%2F%2Fwww.mediafusion.web.id"
+				}
+			]
+		  }
